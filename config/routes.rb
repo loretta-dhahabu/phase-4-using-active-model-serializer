01c4c485b12c13c825b2109907f47e839a781283
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-28 20:00:26 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+
+  #displays just one movie summary
+  get '/movies/:id/summary', to: 'movies#summary'
+
+  #render full collection of movies
+  get '/movie_summaries', to: 'movies#summaries'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
